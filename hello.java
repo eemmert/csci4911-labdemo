a899320f539b98f1041792c1b5e8cc27a3242f1a
eemmert <eemmert@my.hpu.edu>
2020-01-19 21:28:35 -1000

    Added method

--- hello.java
+++ hello.java
@@ -1,1 +1,14 @@
-System.out.print("hello");
+public class hello {
+
+	public static void main(String args[]){
+		String msg = "hello";
+		greetings(msg);
+	}
+
+
+	public static void greetings(String msg) {
+		System.out.print(msg);	
+	}
+
+}
+
